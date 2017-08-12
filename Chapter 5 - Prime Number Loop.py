b8ca25d41b52fef7499461381eb4d250b4854c76
ScottSko <30943111+ScottSko@users.noreply.github.com>
2017-08-11 21:31:11 -0600

    Update Chapter 5 - Prime Number Loop.py

--- Chapter 5 - Prime Number Loop.py
+++ Chapter 5 - Prime Number Loop.py
@@ -2,7 +2,7 @@ num = int(input("Please enter a number: "))
 
 
 if num > 1:
-    # check for factors
+    
     for i in range(2, num):
         if (num % i) == 0:
             print(num, "is not a prime number")
@@ -11,7 +11,5 @@ if num > 1:
     else:
         print(num, "is a prime number")
 
-# if input number is less than
-# or equal to 1, it is not prime
 else:
     print(num, "is not a prime number")
